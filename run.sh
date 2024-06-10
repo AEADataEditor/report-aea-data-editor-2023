e76392a162cbb957c576bb6573fa4ba65910ead0
Lars Vilhuber <lars@vilhuber.com>
2024-06-09 23:24:55 -0400

    Update run.sh for #1

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-PWD=$(pwd)
-repo=${PWD##*/}
-space=aeadataeditor
+
 case $USER in
   vilhuber|larsvilhuber)
 	  WORKSPACE=$(pwd)
@@ -10,27 +8,21 @@ case $USER in
   WORKSPACE=/workspaces
   ;;
 esac
+
+PWD=$(pwd)
+. ${PWD}/.myconfig.sh
+tag=${1:-2023-12-05}
+case $USER in
+  codespace)
+  WORKSPACE=/workspaces
+  ;;
+  *)
+  WORKSPACE=$PWD
+  ;;
+esac
   
-# build the docker if necessary
+# pull the docker if necessary
 
-BUILD=yes
-arg1=$1
-
-docker pull $space/$repo 
-if [[ $? == 1 ]]
-then
-  ## maybe it's local only
-  docker image inspect $space/$repo > /dev/null
-  [[ $? == 0 ]] && BUILD=no
-else
-  BUILD=NO
-fi
-# override
-[[ "$arg1" == "force" ]] && BUILD=yes
-
-if [[ "$BUILD" == "yes" ]]; then
-docker build . -t $space/$repo
-nohup docker push $space/$repo &
-fi
+docker pull $space/$repo:$tag
 
 docker run -v $WORKSPACE:/home/rstudio --entrypoint="/bin/bash" -w /home/rstudio/programs -t --rm $space/$repo ./run_all.sh
