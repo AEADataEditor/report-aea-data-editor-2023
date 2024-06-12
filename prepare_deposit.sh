8cfaca5d42703e0fc9f1e1e0f789418acc0cc67f
Lars Vilhuber <lars.vilhuber@cornell.edu>
2024-06-12 13:20:05 -0400

    Updating README and adjusting package script

--- prepare_deposit.sh
+++ prepare_deposit.sh
@@ -8,7 +8,7 @@ zip -rp $outfile programs/
 zip -rp $outfile images/*
 zip -rp $outfile data/jira/anon  
 zip -rp $outfile data/icpsr 
-zip -rp $outfile data/manual/ 
+zip -rp $outfile data/registry/ 
 zip -rp $outfile data/scholarone/*
 zip -rp $outfile data/zenodo/*
 zip -rp $outfile $(find data -name README.\*)
@@ -16,4 +16,6 @@ zip -rp $outfile *.sh
 zip -rp $outfile *.R
 zip -rp $outfile *.Rproj
 zip -rp $outfile LICENSE.txt
+zip -rp $outfile requirements.txt
 zip -rp $outfile Dockerfile
+zip -rp $outfile .*.sh
